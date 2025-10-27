5db87edec775f7f5374ef926ba82cc3f0e32cd9f
da-harshilv14 <202301208@dau.ac.in>
2025-10-28 02:09:49 +0530

    Update API base URL to use environment variable

--- src/Components/User_Profile/api.js
+++ src/Components/User_Profile/api.js
@@ -1,10 +1,10 @@
-// src/api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/profile",
-    // baseURL: "https://krushisetu-backend-production-4a02.up.railway.app/api",
-    withCredentials: true,
+
+  baseURL: `${import.meta.env.VITE_BASE_URL}/profile`,
+
+  withCredentials: true,
 });
 
 export default api;
