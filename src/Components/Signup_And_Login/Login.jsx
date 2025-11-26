601f4137ca3c0423f4f8a5aa1ca844efd3acaa66
desairudra <202301208@dau.ac.in>
2025-11-26 15:50:32 +0530

    Update Login.jsx

--- src/Components/Signup_And_Login/Login.jsx
+++ src/Components/Signup_And_Login/Login.jsx
@@ -176,6 +176,11 @@ function Login({ onForgotPasswordClick, redirectTo }) {
             setLoginMobile('');
             setLoginOtp('');
             const normalizedRole = normalizeRole(role);
+            console.log("Storing tokens and role:", {
+                access: response.data.access,
+                refresh: response.data.refresh,
+                role: normalizedRole,
+            });
             storeTokens({
                 access: response.data.access,
                 refresh: response.data.refresh,
